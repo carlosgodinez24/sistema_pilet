b9c890a4c80486ae3c9d16d5f2f6d0937404abc0
Carlos <Carlos@CARLOS>
2016-11-01 01:16:13 -0600

    Permisos funcionando al 100% (modulos integrados) y BD actualizada

--- Sistema_Pilet/src/main/java/com/sv/udb/recursos/database.sql
+++ Sistema_Pilet/src/main/java/com/sv/udb/recursos/database.sql
@@ -60,31 +60,24 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `BITACORA`
 (
-	`codi_bita` INT NOT NULL AUTO_INCREMENT,
-	 `codi_usua` INT NULL,
-	 `nive_bita` VARCHAR(10) NOT NULL,
-	 `logg_bita` VARCHAR(50) NOT NULL,
-	 `nomb_bita` VARCHAR(100) NULL,
-	 `acci_bita` VARCHAR(1000) NOT NULL,
-	 `fech_bita` VARCHAR(50) NOT NULL,
-	 PRIMARY KEY(codi_bita)
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `APPLOG`(
-    DATED   varchar(50)    NOT NULL,
-    LOGGER  VARCHAR(50)    NOT NULL,
-    LEVEL   VARCHAR(10)    NOT NULL,
-    MESSAGE VARCHAR(1000)  NOT NULL
+    `codi_bita` INT NOT NULL AUTO_INCREMENT,
+     `codi_usua` INT NULL,
+     `nive_bita` VARCHAR(10) NOT NULL,
+     `logg_bita` VARCHAR(50) NOT NULL,
+     `nomb_bita` VARCHAR(100) NULL,
+     `acci_bita` VARCHAR(1000) NOT NULL,
+     `fech_bita` VARCHAR(50) NOT NULL,
+     PRIMARY KEY(codi_bita)
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `NOTIFICACION`(
-codi_noti INT NOT NULL AUTO_INCREMENT,
-codi_usua INT NOT NULL,
-mens_noti VARCHAR(500) NOT NULL,
-esta_noti INT NOT NULL,
-path_noti VARCHAR(150),
+    `codi_noti` INT NOT NULL AUTO_INCREMENT,
+    `codi_usua` INT NOT NULL,
+    `mens_noti` VARCHAR(500) NOT NULL,
+    `esta_noti` INT NOT NULL,
+    `path_noti` VARCHAR(150),
+    `modu_noti` VARCHAR(200) NOT NULL,
 PRIMARY KEY(codi_noti)
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -105,6 +98,26 @@ INSERT INTO rol values(4,'Secretaria','el ayno',1);
 INSERT INTO usuario values(1,'demo@ricaldone.edu.sv',1);
 
 INSERT INTO usuario_rol values(1,1,1,1);
+
+INSERT INTO `permiso` (`codi_perm`, `desc_perm`, `dire_perm`, `refe_perm`, `esta_perm`) VALUES
+(1, 'App', '/Sistema_Pilet/poo', NULL, 1),
+(2, 'Modulo_usuarios', '/Modulo_usuarios', 1, 1),
+(3, 'Roles', '/roles.xhtml', 2, 1),
+(4, 'Usuario Rol', '/usuariorol.xhtml', 2, 1),
+(5, 'Permiso Rol', '/permisorol.xhtml', 2, 1),
+(6, 'Agregar', '/agregar', 3, 1),
+(7, 'Modificar', '/modificar', 3, 1),
+(8, 'Cambiar estado', '/estado', 3, 1);
+
+INSERT INTO `permiso_rol` (`codi_perm_role`, `codi_perm`, `codi_role`, `esta_perm_role`) VALUES
+(1, 1, 1, 1),
+(2, 5, 1, 1),
+(3, 3, 1, 1),
+(4, 4, 1, 1),
+(5, 2, 3, 1),
+(6, 6, 1, 1),
+(7, 7, 1, 1),
+(8, 8, 1, 1);
 
 DROP FUNCTION IF EXISTS SPLIT_STR;
 CREATE FUNCTION SPLIT_STR(
@@ -134,18 +147,15 @@ delimiter //
 CREATE FUNCTION IF NOT EXISTS GET_URL_PATH (codi int)
 RETURNS VARCHAR(1000) DETERMINISTIC
 BEGIN
-	SET @resp = (SELECT GROUP_CONCAT(ps.dire_perm SEPARATOR '') AS path
-	FROM (
-    SELECT
-        @r AS _id,
-        (SELECT @r := refe_perm FROM permiso WHERE codi_perm = _id) AS codi_perm,
-        @l := @l + 1 AS lvl
-    FROM
-        (SELECT @r := codi, @l := 0) vars,
-        permiso p
-    WHERE @r <> 0) t1
-JOIN permiso ps
-ON t1._id = ps.codi_perm);
-RETURN @resp;
+	DECLARE path VARCHAR(1000);
+	DECLARE temp VARCHAR(1000);
+	DECLARE padre int;
+	SET path = "";
+	SELECT p.dire_perm, p.refe_perm INTO path, padre FROM permiso p WHERE p.codi_perm = codi;
+	WHILE(padre <> 0) DO
+	SELECT p.dire_perm, p.refe_perm INTO temp, padre FROM permiso p WHERE p.codi_perm = padre;
+	SET path = CONCAT(temp,path);
+     END WHILE;
+RETURN path;
 END; //
 delimiter ;
