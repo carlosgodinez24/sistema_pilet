d2d3522c059c8da21befca29a2c8217078e6a232
Alvin <Alvin@192.168.0.18>
2016-11-16 00:01:15 -0600

    Eventos Corregidos

--- Sistema_Pilet/src/main/java/com/sv/udb/ejb/EventoFacade.java
+++ Sistema_Pilet/src/main/java/com/sv/udb/ejb/EventoFacade.java
@@ -1,9 +1,11 @@
 package com.sv.udb.ejb;
 
 import com.sv.udb.modelo.Evento;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 
 /**
@@ -24,6 +26,14 @@ public class EventoFacade extends AbstractFacade<Evento> implements EventoFacade
 
     public EventoFacade() {
         super(Evento.class);
+    }
+    
+    public List<Evento> findByEsta(int estaEven)
+    {
+        TypedQuery<Evento> q = (TypedQuery<Evento>) getEntityManager().createQuery("SELECT e FROM Evento e WHERE e.estaEven = :estaEven");      
+        q.setParameter("estaEven", estaEven);
+        List resu = q.getResultList();
+        return resu.isEmpty() ? null : resu;
     }
     
 }
